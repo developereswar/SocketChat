db060ed0507c7792c254e42fd57a5fe7082e43cd
eswaresh ms <eswaresh.ms@exilant.com>
2018-10-11 12:41:40 +0530

    Half Chat Completed

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+var users = [];
+var connections = [];
 
 // set view engine
-app.set('view engine', 'ejs');
-
+app.set("view engine", "ejs");
 // middlewares
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 //routes
-app.get('/', (req, res) => {
-	res.render('index')
-})
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 //Listen on port 3000
-server = app.listen(3000)
+http.listen(3000);
+
+io.on("connection", socket => {
+  console.log("a user connected");
+
+  //   Sent MSg
+  socket.on("chat_msg", msg => {
+    io.emit("chat_msg", msg);
+  });
+
+  // connect Users
+  socket.emit("some event", { for: "everyone" });
+
+  socket.on("new user", data => {
+    if (data) users.push(data);
+    //   Pass Users list In 
+    socket.emit("get users", { data: users });
+  });
 
 
 
-//socket.io instantiation
-const io = require("socket.io")(server)
-
-io.on('connection', (socket) => {
-    console.log(socket)
-
-    setTimeout(() => {socket.emit('test', { description: 'A custom event named testerEvent!'})}, 2000)
-
-    socket.on('disconnect', ()=>{
-        console.log("disconneced")
-    })
-    
+  socket.on("disconnect", function(data) {
+    console.log("user disconnected");
+     users.splice(users.indexOf(data),1)
+      console.log(data)
+  });
 });
--- public/chat.js
+++ public/chat.js
@@ -1,11 +1,43 @@
 $(() => {
-    var socket = io.socket('http://localhost:3000');
+  var socket = io();
+  var textBox = $("#Txt");
+  var usersname = $("#user");
+  var usersubmit = $("#submit");
 
-    var msg = $('#msg');
+  // Send Msg
+  textBox.keypress(function(event) {
+    var keycode = event.keyCode ? event.keyCode : event.which;
+    if (keycode == "13") {
+      socket.emit("chat_msg", textBox.val());
+      textBox.val("");
+    }
+  });
 
-    socket.on('test', (data)=> {
-        document.write(data.description);
-    })
-})
+  //Get Users Msg
+  socket.on("chat_msg", (msg) => {
+    if (msg) {
+      $(".msg_history").append(
+        '<div class="outgoing_msg">' +
+          '<div class="sent_msg">' +
+          "<p>" +
+          msg +
+          "</p>" +
+          "</div>" +
+          "</div>"
+      );
+    }
+  });
 
-// https://www.youtube.com/watch?v=tHbCkikFfDE
+  //Set User Name
+  $("#submit").click(() => {
+    let username = usersname.val();
+    if (username) {
+      socket.emit("new user", username);
+      $(".usercard").hide();
+    }
+  });
+
+  socket.on("get users", (data) => {
+     console.log(data)
+  });
+});
